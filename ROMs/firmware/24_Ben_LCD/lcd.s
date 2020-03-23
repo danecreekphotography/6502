3cbf4910cbfc010fc55195e2c1a384f6288e623d
Neil Enns <info@danecreekphotography.com>
2020-03-22 19:35:24 -0700

    Initial version using STA INC DEC

--- ROMs/firmware/24_Ben_LCD/lcd.s
+++ ROMs/firmware/24_Ben_LCD/lcd.s
@@ -1,8 +1,16 @@
 ; Implements the long and inefficient "hello world" assembly
 ; program from https://www.youtube.com/watch?v=FY3zTUaykVo
-E = %10000000
+E = %00000001
 RW = %01000000
 RS = %00100000
+
+LCD_FUNCTION_SET        = %00100000 ; Set display functions
+LCD_FUNCTION_1_LINE     = %00000000 ; 1-line display
+LCD_FUNCTION_2_LINE     = %00001000 ; 2-line display
+LCD_FUNCTION_5x8_FONT   = %00000000 ; 5x8 dots font
+LCD_FUNCTION_5x10_FONT  = %00000100 ; 5x10 dots font
+LCD_FUNCTION_8_BIT      = %00010000 ; 8-bit mode
+LCD_FUNCTION_4_BIT      = %00000000 ; 4-bit mode
 
       .setcpu "65C02"
       .include "via.inc"
@@ -16,154 +24,119 @@ RS = %00100000
       .code
 
 init:
-      lda #%11100000    ; Set top three pins on VIA2 Port A to output
+      lda #(E | RW | RS) ; Set VIA2 Port A output pins to output
       sta VIA2_DDRA
-      lda #%11111111    ; Set all pins on VIA2 Port B to output
+      lda #%11111111     ; Set all pins on VIA2 Port B to output
       sta VIA2_DDRB
 
       lda #%00111000    ; Set 8-bit mode, 2-line display, 5x8 font
       sta VIA2_PORTB
-      lda #0            ; Clear RS/RW/E bits
-      sta VIA2_PORTA
-      lda #E            ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #0            ; Clear RS/RW/E bits
-      sta VIA2_PORTA
+      stz VIA2_PORTA    ; Clear RS/RW/E bits
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
       
-      lda #%00001110 ; Display on; cursor on; blink off
+      lda #%00001110    ; Display on; cursor on; blink off
       sta VIA2_PORTB
-      lda #0         ; Clear RS/RW/E bits
-      sta VIA2_PORTA
-      lda #E         ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #0         ; Clear RS/RW/E bits
-      sta VIA2_PORTA
+      stz VIA2_PORTA    ; Clear RS/RW/E bits
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
-      lda #%00000110 ; Increment and shift cursor; don't shift display
+      lda #%00000110    ; Increment and shift cursor; don't shift display
       sta VIA2_PORTB
-      lda #0         ; Clear RS/RW/E bits
-      sta VIA2_PORTA
-      lda #E         ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #0         ; Clear RS/RW/E bits
-      sta VIA2_PORTA
+      stz VIA2_PORTA    ; Clear RS/RW/E bits
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('H')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('e')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('l')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('l')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('o')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #(',')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #(' ')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('w')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('o')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('r')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('l')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('d')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
       lda #('!')
       sta VIA2_PORTB
       lda #RS         ; Set RS; Clear RW/E bits
       sta VIA2_PORTA
-      lda #(RS | E)   ; Set E bit to send instruction
-      sta VIA2_PORTA
-      lda #RS         ; Clear E bits
-      sta VIA2_PORTA
+      inc VIA2_PORTA    ; Toggle E bit
+      dec VIA2_PORTA
 
 loop:
       jmp loop
